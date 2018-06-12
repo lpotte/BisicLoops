d504f17d35a5cfe47aca6f7a3913b60fef40f9ff
lpotte <lpotte@uninorte.edu.co>
2018-06-11 19:38:08 -0500

    version 1.0

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -15,7 +15,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Programa de prueba");
         
     }
     
